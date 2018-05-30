4f6f6d897c3e36cfee6e2aa41eee103c10231c26
xhh4215 <18734184215@163.com>
2018-05-30 15:51:25 +0800

    该程序是练习如何创建项目并且在actiivty跳转和传递信息

---
+++ app/src/main/java/xhh/example/com/fristproject/DisplayMessageActivity.kt
@@ -0,0 +1,14 @@
+package xhh.example.com.fristproject
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_display_message.*
+
+class DisplayMessageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_display_message)
+        textView2.text = intent.getStringExtra("message")
+    }
+}
---
+++ app/src/main/java/xhh/example/com/fristproject/MainActivity.kt
@@ -0,0 +1,21 @@
+package xhh.example.com.fristproject
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+//使用const 将变量编译为编译器常量 就是在编译的时候变量的数值就已经确定了
+const  val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        button.setOnClickListener {
+            val intent = Intent(this@MainActivity,DisplayMessageActivity::class.java)
+            intent.putExtra("message", EXTRA_MESSAGE)
+            startActivity(intent)
+        }
+
+    }
+}
